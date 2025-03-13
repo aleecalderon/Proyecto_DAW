efe49a3f553af8d763903fa4f75e6451e4759f18
Pablo González Jr. <pagc269@gmail.com>
2025-03-13 01:01:32 -0600

    menu sidebar

--- HTML/JS/mapasv.js
+++ HTML/JS/mapasv.js
@@ -22,3 +22,9 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+const sidebar = document.getElementById("sidebar");
+const toggleBtn = document.getElementById("toggle-btn");
+
+toggleBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+});
